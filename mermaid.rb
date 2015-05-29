4d17aa6aa480657f701c94a67dc6a3615061e1d6
Phil Cooper-King <phil@cooperking.net>
2015-05-29 19:50:05 +0100

    Tweaking speed and size of mermaid.

--- mermaid.rb
+++ mermaid.rb
@@ -6,29 +6,34 @@ class Mermaid < Entity
 
 	attr_accessor :game
 
+
 	def initialize(game)
 		super()
 		@game = game
 		@name = :mermaid
+		@width = 50
+		@height = 20
+		@speed = 7
 	end
+
 
 	def update
 
 		# movement
 		if Gosu::button_down? Gosu::KbRight then
-			@x += 10
+			@x += @speed
 		end
 
 		if Gosu::button_down? Gosu::KbLeft then
-			@x -= 10
+			@x -= @speed
 		end
 
 		if Gosu::button_down? Gosu::KbUp then
-			@y -= 10
+			@y -= @speed
 		end
 
 		if Gosu::button_down? Gosu::KbDown then
-			@y += 10
+			@y += @speed
 		end
 
 		# keep in bounds of screen.
